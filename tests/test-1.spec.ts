91774d526ea3e8ce01af7b15021668c0ac92d18a
Eugene Smerdov <eugenesmerdov@MacBook-Pro-Vadim.local>
2026-04-02 21:47:44 +0200

    Initial commit

---
+++ tests/test-1.spec.ts
@@ -0,0 +1,21 @@
+import { test, expect } from '@playwright/test';
+
+test('пллпл', async ({ page }) => {
+  await page.goto('https://playwright.dev/');
+  await expect(page.getByRole('link', { name: 'Playwright logo Playwright' })).toBeVisible();
+  await expect(page.getByLabel('Main', { exact: true }).locator('b')).toHaveAttribute('href', '/');
+  await expect(page.getByRole('link', { name: 'Docs' })).toBeVisible();
+  await expect(page.getByRole('link', { name: 'GitHub repository' })).toBeVisible();
+  await expect(page.getByRole('link', { name: 'Discord server' })).toBeVisible();
+  await expect(page.getByRole('button', { name: 'Switch between dark and light' })).toBeVisible();
+});
+
+test('ss', async ({ page }) => {
+  await page.goto('https://playwright.dev/');
+  await expect(page.getByRole('link', { name: 'Playwright logo Playwright' })).toBeVisible();
+  await expect(page.getByLabel('Main', { exact: true }).locator('b')).toContainText('Playwright');
+  await expect(page.getByRole('link', { name: 'Docs' })).toBeVisible();
+  await expect(page.getByRole('link', { name: 'GitHub repository' })).toBeVisible();
+  await expect(page.getByRole('link', { name: 'Discord server' })).toBeVisible();
+  await expect(page.getByRole('button', { name: 'Switch between dark and light' })).toBeVisible();
+});
