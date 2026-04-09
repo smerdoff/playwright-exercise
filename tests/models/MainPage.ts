9a3f92ede0c6390ea99139def730af69548dfe6c
Yauheni Smerdau <eugenesmerdov17@gmail.com>
2026-04-09 22:40:15 +0200

    added pageObject

---
+++ tests/models/MainPage.ts
@@ -0,0 +1,131 @@
+import { test, expect, Locator, Page } from '@playwright/test';
+
+interface Elements {
+  locator: (page: Page) => Locator;
+  name?: string;
+  text?: string;
+  attribute?: {
+    type: string;
+    value: string;
+  };
+}
+
+export class MainPage {
+  readonly page: Page;
+  readonly elements: Elements[];
+
+  constructor(page: Page) {
+    this.page = page;
+    this.elements = [
+      {
+        locator: (page: Page): Locator =>
+          page.getByRole('link', { name: 'Website for automation' }),
+        name: 'Logo',
+        attribute: {
+          type: 'href',
+          value: '/',
+        },
+      },
+      {
+        locator: (page: Page): Locator => page.locator('.shop-menu a[href="/"]'),
+        name: 'Home tab',
+        text: ' Home',
+        attribute: {
+          type: 'href',
+          value: '/',
+        },
+      },
+      {
+        locator: (page: Page): Locator => page.locator('.shop-menu a[href="/products"]'),
+        name: 'Products tab',
+        text: ' Products',
+        attribute: {
+          type: 'href',
+          value: '/products',
+        },
+      },
+      {
+        locator: (page: Page): Locator => page.locator('.shop-menu a[href="/view_cart"]'),
+        name: 'Cart tab',
+        text: ' Cart',
+        attribute: {
+          type: 'href',
+          value: '/view_cart',
+        },
+      },
+      {
+        locator: (page: Page): Locator => page.locator('.shop-menu a[href="/login"]'),
+        name: 'Login tab',
+        text: ' Signup / Login',
+        attribute: {
+          type: 'href',
+          value: '/login',
+        },
+      },
+      {
+        locator: (page: Page): Locator => page.locator('.shop-menu a[href="/test_cases"]'),
+        name: 'Test Cases tab',
+        text: ' Test Cases',
+        attribute: {
+          type: 'href',
+          value: '/test_cases',
+        },
+      },
+      {
+        locator: (page: Page): Locator => page.locator('.shop-menu a[href="/api_list"]'),
+        name: 'API tab',
+        text: ' API Testing',
+        attribute: {
+          type: 'href',
+          value: '/api_list',
+        },
+      },
+      {
+        locator: (page: Page): Locator => page.getByText('Video Tutorials'),
+        name: 'Video Tutorials tab',
+        text: ' Video Tutorials',
+        attribute: {
+          type: 'href',
+          value: 'https://www.youtube.com/c/AutomationExercise',
+        },
+      },
+      {
+        locator: (page: Page): Locator => page.locator('.shop-menu a[href="/contact_us"]'),
+        name: 'Contact Us tab',
+        text: ' Contact us',
+        attribute: {
+          type: 'href',
+          value: '/contact_us',
+        },
+      },
+    ];
+  }
+  async openMainPage() {
+    await this.page.goto('https://automationexercise.com/');
+  }
+  async checkElementsVisability() {
+    for (const { locator, name } of this.elements) {
+      test.step(`Check ${name} element to visability`, async () => {
+        await expect.soft(locator(this.page)).toBeVisible();
+      });
+    }
+  }
+  async checkElementsText() {
+    for (const { locator, name, text } of this.elements) {
+      if (text) {
+        test.step(`Check text of the ${name} tab`, async () => {
+          await expect(locator(this.page)).toContainText(text);
+        });
+      }
+    }
+  }
+  async checkElementsHrefAttribute() {
+    for (const { locator, name, attribute } of this.elements) {
+      if (attribute) {
+        test.step(`Check href of the ${name} tab`, async () => {
+          await expect(locator(this.page)).toHaveAttribute(attribute.type, attribute.value);
+        });
+      }
+    }
+  }
+}
